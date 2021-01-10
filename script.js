715b8865c377fc6e1910ebb6480c5994e5ae716a
zmrzl <zmrzl@protonmail.com>
2021-01-10 14:08:21 -0500

    refactors original implementation

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
-let msgWaiting = document.querySelector(".message-waiting");
-let searchForm = document.getElementById("search-form");
-
+const msgWaiting = document.querySelector(".message-waiting");
+const searchForm = document.getElementById("search-form");
 const booksDisplay = document.getElementById("books-display");
 
-let getGoogleBooks = "https://www.googleapis.com/books/v1/volumes?q=";
-
-let apiKey = "&key=AIzaSyCw6aeDwd2srAlaiVBFSSBvyl2O7atCTaA";
+const googleBooksURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const apiKey = "&key=AIzaSyCw6aeDwd2srAlaiVBFSSBvyl2O7atCTaA";
 
 searchForm.addEventListener("submit", () => {
   msgWaiting.style.display = "none";
-  let searchInput = document.getElementById("searchInput").value;
-  let searchResult = `${getGoogleBooks}${searchInput}${apiKey}`;
+  const searchInput = document.getElementById("search-input").value;
+  const searchQuery = `${googleBooksURL}${searchInput}${apiKey}`;
 
-  async function getBooks() {
-    const booksPromise = await fetch(searchResult);
-    const getBook = await booksPromise.json();
-    booksDisplay.innerHTML = "";
-    getBook.items.map((book) => {
-      console.log(book.volumeInfo);
+  getBooks(searchQuery);
+});
 
-      let title = book.volumeInfo.title;
-      let authors = book.volumeInfo.authors;
-      let previewLink = book.volumeInfo.previewLink;
-      let publishdate = book.volumeInfo.publishedDate;
-      let publisher = book.volumeInfo.publisher;
-      let smallThumbnail = book.volumeInfo.imageLinks.smallThumbnail;
+async function getBooks(query) {
+  const booksPromise = await fetch(query);
+  const booksData = await booksPromise.json();
 
-      let div = document.createElement("div");
-      div.innerHTML = `
-      
-      <h3>  ${title} </h3>
-      <div class= "book-img">
-       <img src= "${smallThumbnail}" alt= "Image book cover "> 
-       </div>
-        <div class = "book-details">
+  displayBooks(booksData.items);
+}
+
+function displayBooks(books) {
+  // clear any previous results
+  booksDisplay.innerHTML = "";
+
+  books.forEach((book) => {
+
+    let volumeInfo = book.volumeInfo;
+    let title = volumeInfo.title || "N/A";
+    let authors = volumeInfo.authors || "N/A";
+    let previewLink = volumeInfo.previewLink || "N/A";
+    let publishdate = volumeInfo.publishedDate || "N/A";
+    let publisher = volumeInfo.publisher || "N/A";
+    let smallThumbnail = getThumbnail(volumeInfo);
+
+    let bookDiv = document.createElement("div");
+
+    bookDiv.classList.add("book-styling");
+    bookDiv.innerHTML = `
+    <h3>  ${title} </h3>
+    <div class= "book-img">
+     <img src= "${smallThumbnail}" alt= "Image book cover ">
+     </div>
+      <div class = "book-details">
         <small> <strong>Author(s)</strong>: ${authors}</small>
         <small> <strong>Publishers</strong>: ${publisher}</small>
         <small> <strong>Published</strong>: ${publishdate}</small>
-        </div>
-        <a href = "${previewLink}" class ="book-link" target="_blank">  Google Books Preview </a>
-         `;
-      div.classList.add("book-styling");
+      </div>
+      <a href = "${previewLink}" class ="book-link" target="_blank">  Google Books Preview </a>
+       `;
 
-      booksDisplay.append(div);
-    });
-  }
-  getBooks();
-});
+    booksDisplay.append(bookDiv);
+  });
+}
+
+function getThumbnail(volumeInfo) {
+    if (volumeInfo.imageLinks && volumeInfo.imageLinks.smallThumbnail) {
+      return volumeInfo.imageLinks.smallThumbnail;
+    } else {
+      // if google books doesn't have a thumbnail, use a placeholder
+      return "https://picsum.photos/130/190";
+    }
+}
